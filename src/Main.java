a3ea423948fd2c1f5828b500b2224fec63521b92
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-25 10:15:01 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main { // class PartyAffiliation
+    public static void main(String[] args) { // main()
         // Create a Scanner object to get user input
         Scanner in = new Scanner(System.in);
 
         // Display a menu and prompt the user for their choice
-        System.out.println("Select your party affiliation:");
+        System.out.println("Select your party affiliation:"); // output "Select your party affiliation (Democrat, republican, Independent):"
         System.out.println("D - Democrat");
         System.out.println("R - Republican");
         System.out.println("I - Independent");
         System.out.print("Enter your choice: ");
 
         // Read the user's choice as a string
-        String choice = in.nextLine();
+        String choice = in.nextLine(); // String Choice == ""
+        // input Choice
 
         // Use of a cascaded if-else structure to determine the response
-        if (choice.equalsIgnoreCase("D")) {
-            System.out.println("You get a Democratic Donkey.");
-        } else if (choice.equalsIgnoreCase("R")) {
-            System.out.println("You get a Republican Elephant.");
-        } else if (choice.equalsIgnoreCase("I")) {
-            System.out.println("You are an Independent Person.");
+        if (choice.equalsIgnoreCase("D")) { //    if Choice == “D” then
+            System.out.println("You get a Democratic Donkey."); // output “You get a Democratic Donkey.”
+        } else if (choice.equalsIgnoreCase("R")) { // if Choice == "R" then
+            System.out.println("You get a Republican Elephant."); // output " You get a Republican Elephant
+        } else if (choice.equalsIgnoreCase("I")) { // if Choice == "I" then
+            System.out.println("You get an Independent Person."); // output "You get an Independent Person"
         } else {
-            System.out.println("You selected 'Other'.");
-        }
-    }
-}
+            System.out.println("You selected 'Other'.");// output "You selected other"
+
+        } // end if
+    } // return
+} // endClass
